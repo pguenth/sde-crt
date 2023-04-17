701de75f37454ce9c9baa34f4503895c6994bfa1
Patrick Günther <info@paddybu.de>
2023-04-17 16:48:03 +0200

    Added radiative transfer with naima

---
+++ naima-test.py
@@ -0,0 +1,237 @@
+import sdesolver as sdes
+
+import inspect
+import time
+
+from grapheval.nodefigure import NodeFigure, NodeFigureFormat
+from grapheval.cache import PickleNodeCache
+from grapheval.node import *
+from grapheval.graph import draw_node_chain
+
+from src.basicnodes import *
+from src.newnodes import *
+from src.radiativenodes import *
+
+from scipy import integrate
+from scipy.optimize import root_scalar
+
+import formats
+
+from numba import njit, f8, carray
+import numba
+
+import inspect 
+import logging
+import ctypes
+import warnings
+
+from astropy import units as u
+from astropy import constants
+
+logging.basicConfig(level=logging.INFO, #filename='log/tests_log_{}.log'.format(sys.argv[1]),
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+def param_from_numerical(dx_adv, delta, sigma, beta_s, r, n_timesteps):
+    """
+    from outdated chains.py
+    """
+    dx_diff = dx_adv / delta
+
+    dt = (dx_adv - dx_diff / 4) / beta_s
+    q = dt / (dx_adv / dx_diff - 0.25)**2
+    assert q > 0
+    assert dt > 0
+    Xsh = dx_adv * (1 - sigma) + sigma * dx_diff
+    Tmax = n_timesteps * dt
+
+    a = beta_s / 2 * (1 + 1 / r)
+    b = beta_s / 2 * (r - 1) / r
+    
+    param_sim = {'beta_s' : beta_s, 'q' : q, 'Xsh' : Xsh, 'dt' : dt, 'Tmax' : Tmax, 'r' : r, 'a' : a, 'b' : b}
+    param_num = {'dx_adv' : dx_adv, 'dx_diff' : dx_diff, 'delta' : delta, 'sigma' : sigma}
+    return param_sim, param_num
+
+@njit(f8(f8, f8, f8, f8))
+def kruells94_beta(x, Xsh, a, b):
+    return a - b * np.tanh(x / Xsh)
+
+@njit(f8(f8, f8, f8))
+def kruells94_dbetadx(x, Xsh, b):
+    return - b / (Xsh * np.cosh(x / Xsh)**2)
+
+@njit(f8(f8, f8, f8, f8, f8))
+def kruells94_kappa_dep(x, Xsh, a, b, q):
+    return q * kruells94_beta(x, Xsh, a, b)**2
+
+@njit(f8(f8, f8, f8, f8, f8))
+def kruells94_dkappadx_dep(x, Xsh, a, b, q):
+    return 2 * q * kruells94_beta(x, Xsh, a, b) * kruells94_dbetadx(x, Xsh, b)
+
+def drift(out, t, x, Xsh, a, b, k_syn, q):
+    # cpp: kruells_shockaccel2_drift_94_2
+
+    v0 = kruells94_dkappadx_dep(x[0], Xsh, a, b, q) + kruells94_beta(x[0], Xsh, a, b)
+    v1 = - (x[1]) * (kruells94_dbetadx(x[0], Xsh, b) / 3 + k_syn * x[1])
+
+    out[0] = v0
+    out[1] = v1
+
+def diffusion(out, t, x, Xsh, a, b, q):
+    # cpp: kruells_shockaccel2_diffusion
+    diffval = np.sqrt(2.0 * kruells94_kappa_dep(x[0], Xsh, a, b, q))
+
+    # here carray is required to reshape the contiguous pointer
+    out_a = carray(out, (2, 2))
+    out_a[0, 0] = diffval
+    out_a[1, 0] = 0
+    out_a[0, 1] = 0
+    out_a[1, 1] = 0
+
+def boundaries(t, x):
+    ###
+    return 0
+    ###
+
+    x_a = carray(x, (2,))
+    if np.abs(x_a[0]) > 0.005:
+        return 1
+    else:
+        return 0
+
+def nosplit(t, x, last_t, last_x):
+    return False
+
+def split(t, x, last_t, last_x):
+    if x[1] / last_x[1] >= 1.8:#1.41:
+        return True
+    else:
+        return False
+
+# gauss/tesla equivalency
+gauss_tesla_eq = (u.G, u.T, lambda x: x / np.sqrt(4 * np.pi / constants.mu0), lambda x: x * np.sqrt(4 * np.pi / constants.mu0))
+
+def k_syn_from_B(B):
+    B_si = B.to("T", equivalencies=[gauss_tesla_eq])
+    k_syn = B**2 / (6 * np.pi * constants.eps0) * (constants.e.si / (constants.m_e * constants.c))**4
+    return (k_syn / u.Unit("kg-1 m-1")).decompose().value
+
+def B_from_k_syn(k_syn):
+    k_syn = k_syn * u.Unit("kg-1 m-1")
+    B = (constants.m_e * constants.c / constants.e.si)**2 * np.sqrt(6 * np.pi * constants.eps0 * k_syn)
+    return B.to("G", equivalencies=[gauss_tesla_eq])
+
+cachedir = "cache"
+figdir = "figures"
+
+name = "naima-test"
+
+T = 200.0
+#t_inj = 0.05
+#T = 24.5
+t_inj = 0.05
+x0 = np.array([0.0, 1.0])
+dt = 0.001
+confine_x=100#0.05
+n_particle = int(T / t_inj)
+
+init = [(i * t_inj, np.copy(x0)) for i in range(n_particle)]
+
+B = 1 * u.mG
+k_syn = k_syn_from_B(B)
+print(k_syn)
+
+parameters_raw, _ = param_from_numerical(dx_adv=0.00053, delta=0.323, sigma=0.95, beta_s=0.06, r=4, n_timesteps=20000)
+parameters = {
+              'k_syn' : 0.0, #k_syn,
+              'x0' : 0,
+              'y0' : 1,
+            } | parameters_raw 
+dt = parameters_raw['dt']
+
+sde = sdes.SDE(2, init, drift, diffusion, boundaries, split)
+sde.set_parameters(parameters)
+
+# duplicate for the unsplitted variant
+sde_nosplit = sdes.SDE(2, init, drift, diffusion, boundaries, nosplit)
+sde_nosplit.set_parameters(parameters)
+
+
+cache = PickleNodeCache(cachedir, name)
+
+obs_at = [T/32, T/16, T / 4, T]
+#obs_at = [22.0, 22.5, 23.0, T]
+solvernode = SDESolverNode('solver', sde=sde, scheme=b'euler', timestep=dt, observation_times=obs_at, nthreads=4, cache=cache, splitted=True, ignore_cache=False, supervise=True)
+
+histo_opts = {'bin_count' : 60, 'cache' : cache, 'ignore_cache' : True, 'label': 'T={T}, splitted: {splitted}', 'plot': 'hist'}
+
+valuesx = {}
+valuesp = {}
+histogramx = {}
+histogramp = {}
+for T_ in obs_at:
+    valuesx[T_] = SDEValuesNode(f'valuesx_{T_}', {'x' : solvernode[T_]['x'], 'weights': solvernode[T_]['weights']}, index=0, T=T_, cache=cache, ignore_cache=True)
+    valuesp[T_] = SDEValuesNode(f'valuesp_{T_}', {'x' : solvernode[T_]['x'], 'weights': solvernode[T_]['weights']}, index=1, T=T_, cache=cache,
+        confine_range=[(0, -confine_x, confine_x)], ignore_cache=True
+    )
+    histogramx[T_] = HistogramNode(f'histox_{T_}', {'values' : valuesx[T_]['values'], 'weights' : valuesx[T_]['weights']}, log_bins=False, normalize='width', **histo_opts)
+    histogramp[T_] = HistogramNode(f'histop_{T_}', {'values' : valuesp[T_]['values'], 'weights' : valuesp[T_]['weights']}, log_bins=True, normalize='density', **histo_opts)
+
+maxpl = PowerlawNode('pl', {'dataset' : histogramp[T]}, plot=True)
+groupp = NodeGroup('gp', parents=histogramp)
+groupx = NodeGroup('gx', parents=histogramx)
+
+energy_range=np.logspace(-5, 14, 100) * u.eV
+edist = HistogramElectronEnergyDistribution('edist', parents={'histogram': histogramp[50.0]}, p_inj = 1e1 * constants.m_e * constants.c, n0=1e52)
+ic_cmb_naima = ICNaima('iccmbnaima', parents={'electron_distribution': edist}, plot=True, energy_range=energy_range, target=['CMB'], label='IC-CMB')
+ic_fir_naima = ICNaima('icfirnaima', parents={'electron_distribution': edist}, plot=True, energy_range=energy_range, target=['FIR'], label='IC-FIR')
+ic_nir_naima = ICNaima('icnirnaima', parents={'electron_distribution': edist}, plot=True, energy_range=energy_range, target=['NIR'], label='IC-NIR')
+synnaima = SynchrotronNaima('synnaima', parents={'electron_distribution': edist}, plot=True, energy_range=energy_range, B=1*u.mG)
+sscnaima = SSCNaima('sscnaima', parents={'electron_distribution': edist}, plot=True, energy_range=energy_range, B=1*u.G, source_size=10 * u.pc)
+sednaima = SEDSum('sedsum', parents=[ic_cmb_naima, ic_fir_naima, ic_nir_naima, synnaima, sscnaima], plot=True, plot_kwargs={'linestyle': 'dotted', 'color': 'k'})
+radiationgroup = NodeGroup('radgroup', parents=[sednaima, ic_cmb_naima, ic_fir_naima, ic_nir_naima, synnaima, sscnaima])
+
+
+import proplot as pplt
+format9 = NodeFigureFormat(
+                subplots={'array': [[1, 2, 5, 6, 9, 9], [3, 4, 7, 8, 9, 9]]},
+                fig_format={'yscale': 'log', 'yformatter': 'log', 'figtitle': 'Comparison of runs with and without split particles'},
+                axs_format=[{'xscale': 'linear', 'xformatter': pplt.SciFormatter(), 'xlabel': '$x$'}] * 4 
+                    + [{'xscale': 'log', 'xformatter': pplt.SciFormatter(), 'xlabel': '$p/p_\\textrm{inj}$'}] * 4
+                    + [{'xscale': 'log', 'xformatter': pplt.SciFormatter(), 'xlabel': 'Energy in eV', 'ylabel': 'Flux in erg/s'}],
+                legends_kw=[{'loc': 'ur', 'ncols': 1}] * 9 
+        )
+format1 = NodeFigureFormat(
+                subplots={'array': [[1]]},
+                fig_format={'yscale': 'log', 'yformatter': 'log', 'figtitle': 'Radiation test with naima'},
+                axs_format=[{'xscale': 'log', 'xformatter': pplt.SciFormatter(), 'xlabel': 'Energy in eV', 'ylabel': 'Flux $\propto$ erg/s'}],
+                legends_kw=[{'loc': 'r', 'ncols': 1}]
+        )
+
+nfig_rad = NodeFigure(format1)
+nfig_rad.add(radiationgroup, 0, instant=False)
+nfig_rad[0].format(ylim=(1e-18, 1e-9))
+nfig_rad.savefig(figdir + '/' + name + '-radiationonly.pdf')
+
+nfig = NodeFigure(format9)
+for i, (_, h) in enumerate(groupx.parents_iter):
+    nfig.add(h, i, instant=False, plot_on='hist')
+for i, (_, h) in enumerate(groupp.parents_iter):
+    nfig.add(h, i + 4, instant=False, plot_on='hist')
+
+nfig.add(maxpl, 7, instant=False)
+nfig.add(radiationgroup, 8, instant=False)
+
+# without splits
+nfig2 = nfig.copy("nosplit", last_kwargs=dict(sde=sde_nosplit, splitted=False))
+
+# figure with both
+nfigboth = NodeFigure.merge([nfig, nfig2])
+nfigboth[7].format(xlim=(1, 1e7))
+
+nfigboth.savefig(figdir + '/' + name + '-both.pdf')
+nfig.savefig(figdir + '/' + name + '.pdf')
+nfig2.savefig(figdir + '/' + name + '-nosplits.pdf')
+
+#import cProfile
+#pr = cProfile.Profile()
+#cProfile.run('kruells9a1_newstyle()', filename="test-cpp-propagation.perf")
--- src/radiativenodes.py
+++ src/radiativenodes.py
@@ -11,6 +11,201 @@ from agnpy.synchrotron.synchrotron import Synchrotron
 from agnpy.compton.synchrotron_self_compton import SynchrotronSelfCompton
 from agnpy.spectra import PowerLaw
 
+from naima.models import InverseCompton, Synchrotron, TableModel
+
+import logging
+
+def _find_extended_limits(clim, nlim):
+    rlim0 = min(clim[0], nlim[0])
+    rlim1 = max(clim[1], nlim[1])
+    return rlim0, rlim1
+
+def extend_axes_limits(ax, xlim=None, ylim=None):
+    xlimold = ax.get_xlim()
+    ylimold = ax.get_ylim()
+
+    if xlim is not None:
+        ax.format(xlim=_find_extended_limits(xlimold, xlim))
+
+    if ylim is not None:
+        ax.format(ylim=_find_extended_limits(ylimold, ylim))
+
+
+class CallableFromHistogram:
+    """
+    Provides a functional interface to histogram-like data
+
+    Parameters
+    ----------
+    edges : array-like
+        Histogram edges
+    data : array-like
+        Histogram values, must be len(edges) - 1
+    """
+    def __init__(self, edges, data, unit_x=None, unit_y=None):
+        self.edges = u.Quantity(edges)
+        self.data = u.Quantity(data)
+
+        self.unit_x = unit_x if unit_x is not None else self.edges.unit
+        self.unit_y = unit_y if unit_y is not None else self.data.unit
+
+        self._edges_nounit = self.edges / self.unit_x
+        self._data_nounit = self.data / self.unit_y
+
+    def __call__(self, x):
+        """
+        excepts input with unit and returns with unit
+        """
+        if x.shape == ():
+            return self._evaluate_one(x / self.unit_x)
+        else:
+            vs = []
+            for x_ in x.flatten():
+                v =  self._evaluate_one(x_ / self.unit_x)
+                vs.append(v)
+            ra = u.Quantity(np.array(vs).reshape(x.shape) *self.unit_y)
+            return ra
+
+    @cache
+    def _evaluate_one(self, x):
+        """
+        excepts input without unit and returns without unit
+        """
+        r = 0
+        for e0, e1, n in zip(self._edges_nounit[:-1], self._edges_nounit[1:], self._data_nounit):
+            if e0 <= x and e1 > x:
+                r = n
+                break
+
+        return r
+
+
+class HistogramElectronEnergyDistribution(EvalNode):
+    """
+    Imitating the naima interface of an electron distribution as node
+    returns a CallableFromHistogram instance that can be directly used.
+
+    callable input unit: Energy
+    callable output unit:  [count]/Energy
+
+    Parents:
+     * histogram
+
+    kwargs:
+     * p_inj (units of momentum)
+     * n0 (dimensionless)
+    """
+    def do(self, parent_data, common, p_inj, n0, **kwargs):
+        p_code_center, dfdp, err, p_code = parent_data['histogram']
+        dndE = n0 * dfdp / (p_inj * constants.c)
+        E = p_code * p_inj * constants.c
+        return CallableFromHistogram(E, dndE)
+
+        # TableModel seems to fail for zeros in the histogram
+        E_center = p_code_center * p_inj * constants.c
+        return TableModel(E_center, dndE)
+
+class NaimaRadiationWrapper(EvalNode):
+    def def_kwargs(self, **kwargs):
+        kwargs = {
+                'plot_kwargs' : {},
+                'factor' : 1,
+                'distance' : 1 * u.kpc,
+            } | kwargs
+
+        return kwargs
+
+
+    def plot(self, data, ax, common, **kwargs):
+        #print('\n\n', self.name, self.get_color())
+        energies, sed, _ = data
+        energies_nounit = energies.value
+        sed_nounit = sed.value
+        min_plot = max(sed_nounit) / 1e3
+        max_plot = max(sed_nounit) * 1e1
+        if not 'color' in kwargs['plot_kwargs']:
+            kwargs['plot_kwargs']['color'] = self.get_color()
+        extend_axes_limits(ax, ylim=(min_plot, max_plot))
+        return ax.plot(energies_nounit, sed_nounit, label=kwargs['label'], **kwargs['plot_kwargs'])[0]
+
+class SynchrotronNaima(NaimaRadiationWrapper):
+    def do(self, parent_data, common, energy_range, B, distance, **kwargs):
+        synmodel = Synchrotron(parent_data['electron_distribution'], B=B)
+        sed = synmodel.sed(energy_range, distance=distance)
+        flux = synmodel.flux(energy_range, distance=distance)
+        logging.info(f"We = {synmodel.compute_We(constants.c**2 * constants.m_e * 1e1, constants.c**2 * constants.m_e * 1e8)}")
+
+        return energy_range, sed, flux
+
+    def def_kwargs(self, **kwargs):
+        kwargs = super().def_kwargs() | {
+                'label' : 'synchrotron'
+            } | kwargs
+
+        return kwargs
+
+class ICNaima(NaimaRadiationWrapper):
+    def do(self, parent_data, common, energy_range, distance, target=['CMB'], **kwargs):
+        icmodel = InverseCompton(parent_data['electron_distribution'], target)
+        sed = icmodel.sed(energy_range, distance=distance)
+        flux = icmodel.flux(energy_range, distance=distance)
+
+        return energy_range, sed, flux
+
+    def def_kwargs(self, **kwargs):
+        kwargs = super().def_kwargs() | {
+                'label' : 'IC'
+            } | kwargs
+
+        return kwargs
+
+class SSCNaima(NaimaRadiationWrapper):
+    def def_kwargs(self, **kwargs):
+        kwargs = super().def_kwargs() | {
+                'source_size' : None,
+                'label' : 'SSC'
+            } | kwargs
+
+        return kwargs
+
+    def do(self, parent_data, common, energy_range, source_size, distance, B, **kwargs):
+        # https://naima.readthedocs.io/en/latest/radiative.html#synchroton-self-compton
+        synmodel = Synchrotron(parent_data['electron_distribution'], B=B)
+        Lsyn = synmodel.flux(energy_range, distance=0 * u.cm)
+        phdens = Lsyn / (4 * np.pi * source_size**2 * constants.c) * 2.24
+
+        icmodel = InverseCompton(parent_data['electron_distribution'], [["SSC", energy_range, phdens]])
+        sed = icmodel.sed(energy_range, distance=distance)
+        flux = icmodel.flux(energy_range, distance=distance)
+
+        return energy_range, sed, flux
+
+
+class SEDSum(NaimaRadiationWrapper):
+    def def_kwargs(self, **kwargs):
+        kwargs = super().def_kwargs() | {
+                'source_size' : None,
+                'label' : 'Total'
+            } | kwargs
+
+        return kwargs
+
+    def do(self, parent_data, common, **kwargs):
+        energy_range, sed_sum, flux_sum = parent_data[0]
+        for energy_range_, sed_, flux_ in parent_data[1:]:
+            if not np.all(energy_range == energy_range_):
+                raise ValueError("energy range mismatch")
+
+            sed_sum += sed_
+            flux_sum += flux_
+
+        return energy_range, sed_sum, flux_sum
+
+            
+
+
+
+
 class MomentumCount(EvalNode):
     def do(self, parent_data, common, **kwargs):
         y, U, err, edges = parent_data
@@ -119,6 +314,7 @@ class SynchrotronBase(EvalNode):
         if not 'color' in kwargs['plot_kwargs']:
             kwargs['plot_kwargs']['color'] = self.get_color()
         return ax.plot(nus_nounit, flux_nounit, label=kwargs['label'], **kwargs['plot_kwargs'])[0]
+
 
 class SynchrotronDeltaApproxAgnPy(SynchrotronBase):
     def flux_from_nu(self, nu, electron_dist, model_params, gamma_integrate):
